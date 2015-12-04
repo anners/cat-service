bd9e1e7ff469b03c77adbac6d69040c6fa54e03c
Ann Wallace <annerz@gmail.com>
2015-12-04 12:18:29 -0800

    added cat api

--- httptest.go
+++ httptest.go
@@ -1,6 +1,8 @@
+//http://thenewstack.io/building-a-web-server-in-go/
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
 )
@@ -10,7 +12,23 @@ func hello(w http.ResponseWriter, r *http.Request) {
 
 }
 
+type Message struct {
+	Text string
+}
+
+func cat(w http.ResponseWriter, r *http.Request) {
+	m := Message{"MEOW, purrrrr"}
+	b, err := json.Marshal(m)
+
+	if err != nil {
+		panic(err)
+	}
+
+	w.Write(b)
+}
+
 func main() {
 	http.HandleFunc("/", hello)
+	http.HandleFunc("/cat", cat)
 	http.ListenAndServe(":8080", nil)
 }
