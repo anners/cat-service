26e6a3776e3d0324ec15362fdf09beb305d61678
Ann Wallace <annerz@gmail.com>
2015-12-08 15:54:33 -0600

    added /catpic - now you can get random catpics

--- httptest.go
+++ httptest.go
@@ -5,6 +5,7 @@ package main
 import (
 	"encoding/json"
 	"io"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -15,6 +16,19 @@ func hello(w http.ResponseWriter, r *http.Request) {
 
 type Message struct {
 	Text string
+}
+
+// catapi NTMzNzg
+func catpic(w http.ResponseWriter, r *http.Request) {
+	catapi := "http://thecatapi.com/api/images/get?format=html"
+	request, err := http.Get(catapi)
+	if err != nil {
+		panic(err)
+	}
+	defer request.Body.Close()
+	content, err := ioutil.ReadAll(request.Body)
+	w.Header().Set("Content-Type", "text/html")
+	w.Write([]byte(content))
 }
 
 func cat(w http.ResponseWriter, r *http.Request) {
@@ -31,5 +45,6 @@ func cat(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", hello)
 	http.HandleFunc("/cat", cat)
+	http.HandleFunc("/catpic", catpic)
 	http.ListenAndServe(":8080", nil)
 }
