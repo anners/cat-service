091657430b82099263775f6ea015102af61dbe07
Ann Wallace <annerz@gmail.com>
2015-12-22 15:07:29 -0800

    updated to use image seach service

--- caas.go
+++ caas.go
@@ -3,31 +3,56 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"io/ioutil"
+	"math/rand"
 	"net/http"
+
 )
+
+type Message struct {
+	Text string
+}
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello world!")
 
 }
 
-type Message struct {
-	Text string
-}
-
-// get random cat pictures
+// display random cat pictures 
 func catpic(w http.ResponseWriter, r *http.Request) {
-	catapi := "http://thecatapi.com/api/images/get?format=html"
+	//catapi := "http://thecatapi.com/api/images/get?format=html"
+	// url for image search
+	catapi := "http://localhost:8888//image?search=cat"
 	request, err := http.Get(catapi)
 	if err != nil {
 		panic(err)
 	}
 	defer request.Body.Close()
+
 	content, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	data := make(map[string][]map[string]string)
+	//var data interface{}
+	err = json.Unmarshal(content, &data)
+	if err != nil {
+        panic(err.Error())
+    }
+
+    // get random image from the data returned
+ 	var image string 
+    randomIndex := rand.Intn(len(data["items"])-1)    	
+    randocat := data["items"][randomIndex] 
+    for _, url := range randocat {
+    	image = url
+    }
+
 	w.Header().Set("Content-Type", "text/html")
-	w.Write([]byte(content))
+	fmt.Fprintf(w, "<img src=%s>", image)
 }
 
 func cat(w http.ResponseWriter, r *http.Request) {
