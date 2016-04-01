b40704b6ae2d71cbb2071817412f172fb241e775
ann wallace <annerz@gmail.com>
2016-04-01 12:30:32 -0700

    april fools

--- caas.go
+++ caas.go
@@ -108,7 +108,7 @@ func pic(w http.ResponseWriter, r *http.Request) {
 
 	if service != nil {
 		// construct the catapi
-		catapi = "http://image-service.service.consul:" + strconv.Itoa(service[0].ServicePort) + "/image?search=metapod"
+		catapi = "http://image-service.service.consul:" + strconv.Itoa(service[0].ServicePort) + "/image?search=donaldtrump"
 	}
 
 	request, err := http.Get(catapi)
