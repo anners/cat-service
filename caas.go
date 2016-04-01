41c5a92952c7ae8d62a8f561c359686fd61616b0
ann wallace <annerz@gmail.com>
2016-04-01 16:31:31 -0700

    cats

--- caas.go
+++ caas.go
@@ -108,7 +108,7 @@ func pic(w http.ResponseWriter, r *http.Request) {
 
 	if service != nil {
 		// construct the catapi
-		catapi = "http://image-service.service.consul:" + strconv.Itoa(service[0].ServicePort) + "/image?search=berniesanders"
+		catapi = "http://image-service.service.consul:" + strconv.Itoa(service[0].ServicePort) + "/image?search=cats"
 	}
 
 	request, err := http.Get(catapi)
